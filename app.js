a53f61b93305e999cffbe2eab6f2498ae6a5f8d5
Philip Scott <pscott@zeptohost.com>
2017-07-06 21:29:05 -0400

    Add node js server side - fully functional

--- app.js
+++ app.js
@@ -8,13 +8,19 @@ app.use(express.static('public'));
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
-/*
-io.connection(function(socket) {
-  socket.on('play or pause', function() {
 
+io.on('connection', function(socket) {
+  socket.on('buffering', function(time) {
+    socket.broadcast.emit('buffering', time);
+  });
+  socket.on('playing', function() {
+    socket.broadcast.emit('playing');
+  });
+  socket.on('paused', function() {
+    socket.broadcast.emit('paused');
   });
 });
-*/
+
 var port = process.env.PORT || 8080;
 http.listen(port, function() {
   console.log('listening on port ' + port);
--- public/client.js
+++ public/client.js
@@ -1,32 +1,76 @@
-//$(function() {
-  var tag = document.createElement('script');
+var player;
+var socket;
+var broadcast = true;
+var tag = document.createElement('script');
 
-  tag.src = 'https://www.youtube.com/iframe_api';
-  var firstScriptTag = document.getElementsByTagName('script')[0];
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+tag.src = 'https://www.youtube.com/iframe_api';
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-  var player;
-  function onYouTubeIframeAPIReady() {
-    console.log('hello');
-    player = new YT.Player('ytVid', {
-      height: '530',
-      width: '942',
-      videoId: 'M7lc1UVf-VE',
-      events: {
-        'onReady': onPlayerReady,
-        'onStateChange': onPlayerStateChange
-      }
-    });
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('ytVid', {
+    height: '530',
+    width: '942',
+    videoId: 'M7lc1UVf-VE',
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+function onPlayerReady(event) {
+  socket = io();
+  socket.on('paused', function() {
+    broadcast = false; 
+    player.pauseVideo();
+    broadcast = true;
+  });
+  socket.on('playing', function() {
+    broadcast = false;
+    player.playVideo();
+    broadcast = true;
+  });
+  //i'll tweak it later
+  socket.on('buffering', function(time) {
+    broadcast = false;
+    player.seekTo(time, true);
+    player.playVideo();
+    broadcast = true;
+  });
+}
+
+function onPlayerStateChange(event) {
+  if (broadcast) {
+    switch(event.data) {
+      case YT.PlayerState.UNSTARTED:
+        socket.emit('unstarted');
+        break;
+      case YT.PlayerState.ENDED:
+        socket.emit('ended');
+        break;
+      case YT.PlayerState.PLAYING:
+        socket.emit('playing');
+        break;
+      case YT.PlayerState.PAUSED:
+        socket.emit('paused');
+        break;
+      case YT.PlayerState.BUFFERING:
+        socket.emit('buffering', player.getCurrentTime());
+        break;
+      case YT.PlayerState.CUED:
+        socket.emit('cued');
+        break;
+    }
   }
+}
 
-  function onPlayerReady(event) {
-  }
+function changeURL() {
+  var url = document.getElementById('url').value;
+  player.loadVideoById(url);
+}
+/*function noBroadcast(fn) {
+  broadcast = false;
+  fn();
+  broadcast = true;
+}*/
 
-  function onPlayerStateChange(event) {
-  }
-
-  function changeURL() {
-    var url = document.getElementById('url').value;
-    player.loadVideoById(url);
-  }
-//});
